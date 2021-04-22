5aa51209fbcaa75d77cdc381f82bd6a7b26f8dca
elliotthebot <81969654+elliotthebot@users.noreply.github.com>
2021-04-23 02:12:35 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -551,9 +551,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'infinitex Mirror Bot Search',
+                                                        author_name='infinitex Mirror Bot',
+                                                        author_url='https://t.me/techxelon',
                                                         html_content=content
                                                         )['path'])
 
